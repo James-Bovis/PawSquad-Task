b8cd331c77c30d5385eb1610843178991d22367f
James-Bovis <hello@jamesbovis.com>
2017-09-07 21:23:00 +0100

    Added gulpfile.js - Configured live reload and "watch" task.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+// Requires Live Reload
+var browserSync = require('browser-sync').create();
+
+// Compiles the sass code into css
+gulp.task('sass', function(){
+  return gulp.src('build/scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'})) // Using gulp-sass - outputs compressed file
+    .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Runs browsersync on root folder
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'build'
+    },
+  })
+})
+
+
+// Gulp watch syntax
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('build/scss/*.scss', ['sass']);
+  // Other watchers
+  gulp.watch('build/*.html', browserSync.reload);
+})
